6ac11b7d0ee46a5f572ced8b78e38175db9661cf
devFNL <fedelindberg@gmail.com>
2023-07-25 02:01:26 -0300

    v1.0
    first version of to-do list

---
+++ main.js
@@ -0,0 +1,51 @@
+const list = document.querySelector("#liste");
+const taskInput = document.querySelector("#taskInput");
+const addButton = document.querySelector("#button");
+const body = document.querySelector("body");
+const swap = document.querySelector(".switch");
+
+function addTask() {
+  const taskText = taskInput.value.trim();
+  if (taskText !== "") {
+    insertItem(taskText);
+    taskInput.value = "";
+  }
+}
+
+addButton.addEventListener("click", addTask);
+
+taskInput.addEventListener("keypress", function (event) {
+  if (event.key === "Enter") {
+    addTask();
+  }
+});
+
+function insertItem(taskText) {
+  const newItem = document.createElement("li");
+  newItem.className = "list-group-item custom-item";
+  newItem.textContent = taskText;
+  list.querySelector("ul").appendChild(newItem);
+}
+
+const isDarkMode = localStorage.getItem("darkMode") === "true" || false;
+updateDarkMode(isDarkMode);
+
+function toggleDarkMode() {
+  const isDarkMode = document.getElementById("darkModeToggle").checked;
+  updateDarkMode(isDarkMode);
+  localStorage.setItem("darkMode", isDarkMode);
+}
+
+function updateDarkMode(isDarkMode) {
+  if (isDarkMode) {
+    body.classList.add("dark-mode");
+    swap.classList.add("dark-mode");
+  } else {
+    body.classList.remove("dark-mode");
+    swap.classList.remove("dark-mode");
+  }
+}
+
+document
+  .getElementById("darkModeToggle")
+  .addEventListener("change", toggleDarkMode);
